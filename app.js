187bae16c3d30602c28465929ef89d822e775f8b
ujjawal16 <ujjawal.golani@gmail.com>
2021-08-31 20:38:48 +0530

    test node

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ const express = require('express');
 const app = express();
 const cookieParser = require('cookie-parser')
 app.use(cookieParser())
-dotenv.config({path:'./config.env'});
-require('./db/conn')
+// dotenv.config({path:'./config.env'});
+// require('./db/conn')
 
 app.use(express.json())
 
