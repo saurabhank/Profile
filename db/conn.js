5696a9cc522722ee2262617338829a8deb476214
ujjawal16 <ujjawal.golani@gmail.com>
2021-08-31 20:50:41 +0530

    test node

--- db/conn.js
+++ db/conn.js
@@ -1,10 +1,10 @@
-const mongoose = require('mongoose');
-const DB=process.env.DATABASE
+// const mongoose = require('mongoose');
+// const DB=process.env.DATABASE
 
-mongoose.connect(DB,{useNewUrlParser:true,useCreateIndex:true,useUnifiedTopology:true,useFindAndModify:false}).then(()=>{
-    console.log("connected")
-}).catch(err=>console.log(err));
-const middleware=(req,res,next)=>{
-    console.log("success")
-    next();
-}
+// mongoose.connect(DB,{useNewUrlParser:true,useCreateIndex:true,useUnifiedTopology:true,useFindAndModify:false}).then(()=>{
+//     console.log("connected")
+// }).catch(err=>console.log(err));
+// const middleware=(req,res,next)=>{
+//     console.log("success")
+//     next();
+// }
